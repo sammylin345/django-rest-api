c13aaf4b24ed75a626893406564d810e7ea989c6
sammylin345 <43788227+sammylin345@users.noreply.github.com>
2022-04-26 22:14:37 -0500

    added all files

---
+++ main/api/serializer.py
@@ -0,0 +1,10 @@
+
+from rest_framework import serializers
+
+from main.models import Book
+
+
+class BookModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
